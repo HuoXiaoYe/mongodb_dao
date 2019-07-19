36a635c8b54c06e1a6171700ce9982ff35327eae
huoxiaoye <17711034@bjtu.edu.cn>
2019-07-19 09:58:57 +0800

    完成了数据的增加

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ class xiaoye_db {
 			callback(err,db)
 		})
 	}
+	insert(collectionName,json,callback){
+		this._connect((err,db)=>{
+			if(err) return console.log("连接数据库失败");
+			let dbase = db.db(this.databaseName);
+			if(!Array.isArray(json)){ // 添加一条数据
+				dbase.collection(collectionName).insertOne(json,(err,result)=>{
+					return callback&&callback()
+				})
+			}
+			// 添加多条数据
+			dbase.collection(collectionName).insertMany(json,(err,result)=>{
+				return callback&&callback()
+			})
+		})
+	}
 }
 
 
@@ -19,7 +34,16 @@ class xiaoye_db {
 
 
 
-let xiaoye = new xiaoye_db("mongodb://localhost:27017")
-xiaoye._connect((err,db)=>{
-	console.log("success")
+let xiaoye = new xiaoye_db("mongodb://localhost:27017","xiaoye")
+// xiaoye._connect((err,db)=>{
+// 	console.log("success")
+// })
+xiaoye.insert("student",{"name":"xiaobai","age":14},(err, result)=>{
+	if(err) return console.log("添加失败")
+	return console.log(123345)
+})
+
+xiaoye.insert("student",[{"name":"xiaobai2","age":14},{"name":"xiaobai3","age":14}],(err, result)=>{
+	if(err) return console.log("添加失败")
+	return console.log(123345)
 })
